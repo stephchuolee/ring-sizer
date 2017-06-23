ed3bda31150ef145ebdd447f115f5f6ec1721084
theabuitendyk <theabuitendyk@gmail.com>
2017-06-23 01:00:07 -0700

    Small refactor for update all rings function.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function() {
   var cardBaseWidth = card.width();
   var arrow = $('.arrow-container');
   var aspectRatio = 312 / 196;
-  updateArrowOffset();
+  updateArrowOffset(); /* Initially position the arrow container diagonally to the bottom of the CC */
 
   card.resizable({
     aspectRatio: aspectRatio,
@@ -13,60 +13,55 @@ $(function() {
   });
 
   card.on('resizestop', function() {
-    updateAllRings(rings, cardBaseWidth, card.width());
+    updateAllRings();
+    updateArrowOffset();
   });
 
   var rings = {
-    "ring-4": 56.15888688,
-    "ring-4-5": 57.71083555,
-    "ring-5": 59.33496789,
-    "ring-5-5": 60.85082473,
-    "ring-6": 62.4027734,
-    "ring-6-5": 63.95472208,
-    "ring-7": 65.57885441,
-    "ring-7-5": 67.08268064,
-    "ring-8": 68.75493542,
-    "ring-8-5": 70.03018006,
-    "ring-9": 71.38963898,
-    "ring-9-5":72.54457753,
-    "ring-10": 74.10855681,
-    "ring-10-5": 75.55222999,
-    "ring-11": 77.11620928,
+    'ring-4': 56.15888688,
+    'ring-4-5': 57.71083555,
+    'ring-5': 59.33496789,
+    'ring-5-5': 60.85082473,
+    'ring-6': 62.4027734,
+    'ring-6-5': 63.95472208,
+    'ring-7': 65.57885441,
+    'ring-7-5': 67.08268064,
+    'ring-8': 68.75493542,
+    'ring-8-5': 70.03018006,
+    'ring-9': 71.38963898,
+    'ring-9-5':72.54457753,
+    'ring-10': 74.10855681,
+    'ring-10-5': 75.55222999,
+    'ring-11': 77.11620928,
   };
 
   $('#increase-btn').on('click', function() {
     card.css('height', 'auto');
     card.css('width', '+=2');
-    updateAllRings(rings, cardBaseWidth, card.width());
+    updateAllRings();
     updateArrowOffset();
   });
 
   $('#decrease-btn').on('click', function() {
     card.css('height', 'auto');
     card.css('width', '-=2');
-    updateAllRings(rings, cardBaseWidth, card.width());
+    updateAllRings();
     updateArrowOffset();
   });
 
-  function updateRing(ring, ringBaseWidth, baseCardWidth, newCardWidth) {
-    var ring = $('.'+ring);
-    var changeRatio = 1 + ((newCardWidth - baseCardWidth) / baseCardWidth);
-    var newRingWidth = ringBaseWidth * changeRatio;
+  function updateAllRings() {
+    var changeRatio = 1 + ((card.width() - cardBaseWidth) / cardBaseWidth);
 
-    ring.css('width', newRingWidth);
-    ring.css('height', newRingWidth);
-  }
-
-  function updateAllRings(rings, baseCardWidth, newCardWidth) {
     for (var key in rings) {
-      updateRing(key, rings[key], baseCardWidth, newCardWidth);
+      var newDiameter = rings[key] * changeRatio;
+      $('.' + key).width(newDiameter).height(newDiameter);
     }
   }
 
   function updateArrowOffset() {
-    var cardPositionLeft = card.offset().left;
-    var offsetArrow = cardPositionLeft + card.width();
-    console.log(offsetArrow);
+    var cardXCoor = card.offset().left;
+    var offsetArrow = cardXCoor + card.width();
+
     arrow.css('left', offsetArrow + 'px');
   }
 })
